005fbebfcfca7d1fadaea8a642eea517fc12b627
Leo Davidson <94934268+ldavidson2@users.noreply.github.com>
2023-02-16 12:25:48 -0600

    added db

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,14 @@
+export type AmplifyDependentResourcesAttributes = {
+  "storage": {
+    "venvdb": {
+      "Arn": "string",
+      "Name": "string",
+      "PartitionKeyName": "string",
+      "PartitionKeyType": "string",
+      "Region": "string",
+      "SortKeyName": "string",
+      "SortKeyType": "string",
+      "StreamArn": "string"
+    }
+  }
+}
