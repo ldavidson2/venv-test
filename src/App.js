c608b05b8af3711a0acac3c9229306a07ec87ad7
Leo Davidson <94934268+ldavidson2@users.noreply.github.com>
2023-02-16 16:40:30 -0600

    Revert "testing fetch"
    This reverts commit 837ef0c2d36c687e950ff9262645ddf33584ccf6.

--- src/App.js
+++ src/App.js
@@ -2,16 +2,11 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Message from "./components/message";
 
-function callApi() {
-  fetch('https://test1API.execute-api.AWS::Region.amazonaws.com/items', { method: 'GET' })
-    .then(data => data.json()) 
-    .then(json => alert(JSON.stringify(json)))
-}
 
 function App() {
   return (
     <div className="App">
-      <button onClick={callApi}>Call API</button>
+      <Message />
     </div>
   );
 }
