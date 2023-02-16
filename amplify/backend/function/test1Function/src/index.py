4da0006b77aa9564cc55a40d796cc42baf4e70aa
Leo Davidson <94934268+ldavidson2@users.noreply.github.com>
2023-02-16 14:44:16 -0600

    changing function

--- amplify/backend/function/test1Function/src/index.py
+++ amplify/backend/function/test1Function/src/index.py
@@ -1,8 +1,13 @@
 import json
+import boto3
+
+dynamodb = boto3.resource('dynamodb', region_name="us-east-2", aws_access_key_id="AKIAX3FHVXYSKFFYPGE7", aws_secret_access_key= "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")
+table = dynamodb.Table('resttest-staging')
 
 def handler(event, context):
   print('received event:')
   print(event)
+  response = table.query(KeyConditionExpression=Key('PK').eq('COMP#0'))
   
   return {
       'statusCode': 200,
@@ -11,5 +16,5 @@ def handler(event, context):
           'Access-Control-Allow-Origin': '*',
           'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
       },
-      'body': json.dumps('Hello from your new Amplify Python lambda!')
+      'body': json.dumps(response)
   }
