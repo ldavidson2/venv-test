99f0d172f329809045d09a9b51a82e49986a4388
Leo Davidson <94934268+ldavidson2@users.noreply.github.com>
2023-02-16 14:29:14 -0600

    changed function

--- amplify/backend/function/test1Function/src/index.py
+++ amplify/backend/function/test1Function/src/index.py
@@ -3,13 +3,6 @@ import json
 def handler(event, context):
   print('received event:')
   print(event)
+  data = 'test'
   
-  return {
-      'statusCode': 200,
-      'headers': {
-          'Access-Control-Allow-Headers': '*',
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
-      },
-      'body': json.dumps('Hello from your new Amplify Python lambda!')
-  }
+  return data
