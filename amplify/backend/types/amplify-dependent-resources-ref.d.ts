29bf28b174c9d06f42501ce2e2a05f6ba02528ed
Leo Davidson <94934268+ldavidson2@users.noreply.github.com>
2023-02-16 13:42:37 -0600

    reconnected to amplify

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,14 +1,1 @@
-export type AmplifyDependentResourcesAttributes = {
-  "storage": {
-    "venvdb": {
-      "Arn": "string",
-      "Name": "string",
-      "PartitionKeyName": "string",
-      "PartitionKeyType": "string",
-      "Region": "string",
-      "SortKeyName": "string",
-      "SortKeyType": "string",
-      "StreamArn": "string"
-    }
-  }
-}
+export type AmplifyDependentResourcesAttributes = {}
