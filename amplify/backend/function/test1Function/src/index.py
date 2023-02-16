ce24cd6168c13a200d7ebdc7d8a318de8118336b
Leo Davidson <94934268+ldavidson2@users.noreply.github.com>
2023-02-16 15:15:14 -0600

    changed function

--- amplify/backend/function/test1Function/src/index.py
+++ amplify/backend/function/test1Function/src/index.py
@@ -1,20 +1,15 @@
 import json
 import boto3
 
-dynamodb = boto3.resource('dynamodb', region_name="us-east-2", aws_access_key_id="AKIAX3FHVXYSKFFYPGE7", aws_secret_access_key= "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")
-table = dynamodb.Table('resttest-staging')
-
 def handler(event, context):
-  print('received event:')
-  print(event)
-  response = table.query(KeyConditionExpression=Key('PK').eq('COMP#0'))
+    dynamodb = boto3.resource('dynamodb', region_name="us-east-2", aws_access_key_id="AKIAX3FHVXYSKFFYPGE7", aws_secret_access_key= "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")
+    table = dynamodb.Table('resttest-staging')
+    print('received event:')
+    print(event)
+    response = table.get_item(
+        Key={
+            'PK': 'COMP#0'
+        }
+    )
   
-  return {
-      'statusCode': 200,
-      'headers': {
-          'Access-Control-Allow-Headers': '*',
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
-      },
-      'body': json.dumps(str(response))
-  }
+    return response['Item']
